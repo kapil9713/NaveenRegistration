c2dca61fabdc92bc3a368b2c37e2d0623ced932f
Kapil Rathore <kapil9713@gmail.com>
2023-04-14 15:41:54 +0530

    make one locators class

---
+++ src/main/java/BrowserUtil/Locators.java
@@ -0,0 +1,21 @@
+package BrowserUtil;
+
+public class Locators {
+
+	public static void main(String[] args) {
+		
+		for(int i=0;i<=10;i++) {
+			if(i%2==0) {
+				System.out.println("Even="+i);
+			}
+			else {
+				System.out.println("Odd="+i);
+			}
+		}
+		
+		
+		
+
+	}
+
+}
